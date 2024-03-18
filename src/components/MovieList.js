c534a801b2ae6ec41c1dd72fb423753fab5f44e3
Isidora Brasancevic <isidorabr@gmail.com>
2024-03-18 17:24:26 +0100

    Add basic pagination

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -7,19 +7,27 @@ import MovieTile from './MovieTile';
 const MovieList = () => {
   const [movies, setMovies] = useState([]);
   const navigate = useNavigate();
+  const [page, setPage] = useState(0);
+  const [pageSize, setPageSize] = useState(2);
 
   function getMovies() {
-    fetch("http://localhost:8080/movies/allMovies", {
+    fetch(`http://localhost:8080/movies/allMovies?page=${page}&size=${pageSize}`, {
+      method: 'GET',
+      credentials: 'same-origin',
+      // headers: this.getHeaders(),
     })
-        .then(response => response.json())
-        .then(data => {
-            setMovies(data);
-        })
+    .then(response => response.json())
+    .then(data => {
+      console.log("Page: ", {page});
+      console.log(data);
+      setMovies(data);
+    })
   }
 
   useEffect(() => {
-    return () => getMovies();
-  }, [])
+    // return () => getMovies();
+    getMovies();
+  }, [page, pageSize])
 
   const movieDetails = (id) => { 
     navigate('/detailed-movie-view/' + id);
@@ -27,12 +35,14 @@ const MovieList = () => {
 
   return (
     <div>
-        <h1>Take a look at our movies...</h1>
-          <div>
-            <ImageList sx={{ width: '100%', height: '100%' }}>
-              <MovieTile movies={movies} movieDetails={movieDetails}/>
-            </ImageList>
-          </div>
+      <h1>Take a look at our movies...</h1>
+      <div>
+        <ImageList sx={{ width: '100%', height: '100%' }}>
+          <MovieTile movies={movies} movieDetails={movieDetails}/>
+        </ImageList>
+      </div>
+      <button onClick={() => setPage(page - 1)} disabled={page === 0}>Previous</button>
+      <button onClick={() => setPage(page + 1)}>Next</button>
     </div>
   )
 }
