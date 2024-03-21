2621720bcd0e164e0e71cc9affa03d0f9519fbda
Isidora Brasancevic <isidorabr@gmail.com>
2024-03-21 18:14:08 +0100

    Update new movie form style

--- src/components/NewMovieForm.js
+++ src/components/NewMovieForm.js
@@ -5,6 +5,7 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Input from '@mui/material/Input';
+import './NewMovieForm.css';
 
 const NewMovieForm = () => {
     const [name, setName] = useState("");
@@ -59,38 +60,35 @@ const NewMovieForm = () => {
     }
 
     return (
-        <div>
-            <form noValidate autoComplete="off">
+        <div className="formContainer">
+            <form className="form1" noValidate autoComplete="off">
                 <div>
                     <Card raised>
                         <CardContent>
                             <img src={imageUrl} style={{ width: '100%' }} alt="no image url"/>
                         </CardContent>
                     </Card>
-                    <br/>
-                    <Input placeholder="Image url" inputProps={{ 'aria-label': 'description' }}
-                        onChange={(e) => setProperty("imageUrl", e.target.value)}
-                    />
+                    <Input placeholder="Image url" sx={{ minWidth: '60%', mt:3 }}
+                        onChange={(e) => setProperty("imageUrl", e.target.value)} />
                 </div>
                 <div>
-                    <h1> <Input placeholder="Title" inputProps={{ 'aria-label': 'description' }}
+                    <Input placeholder="Title" sx={{ minWidth: '60%', mt:2 }}
                         value={name}
-                        onChange={(e) => setProperty("name", e.target.value)} /> </h1>
-                    <h1> <Input placeholder="Genre" inputProps={{ 'aria-label': 'description' }}
+                        onChange={(e) => setProperty("name", e.target.value)} />
+                        
+                    <Input placeholder="Genre" sx={{ minWidth: '60%', mt:2 }}
                         value={genre}
-                        onChange={(e) => setProperty("genre", e.target.value)} /> </h1>
-                    <h1> <Input placeholder="Year" input type="number" inputProps={{ 'aria-label': 'description' }}
+                        onChange={(e) => setProperty("genre", e.target.value)} />
+
+                    <Input placeholder="Year" sx={{ minWidth: '60%', mt:2 }} type="number" 
                         value={year}
-                        onChange={(e) => setProperty("year", e.target.value)} /> </h1>
+                        onChange={(e) => setProperty("year", e.target.value)} />
 
-                    <br />
-
-                    <TextField id="outlined-basic" label="Description" variant="outlined" multiline rows={10}
+                    <TextField id="outlined-basic" sx={{ mt:4 }} label="Description" variant="outlined" multiline rows={10}
                         value={description}
                         onChange={(e) => setProperty("description", e.target.value)} />
-                    <br />
-                    <br />
-                    <Button variant="outlined" onClick={ addMovie }>Done!</Button>
+                    <br/>
+                    <Button className="button1" variant="outlined" sx={{ mt:2 }} onClick={ addMovie }>Done!</Button>
                 </div>
             </form>
         </div>
